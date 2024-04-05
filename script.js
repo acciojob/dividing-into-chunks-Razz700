fb3057e087ad7886f6a9724218e15f93a7b9d11e
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-06 00:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,18 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	let x=[[],[],[]];
+let j=0,i=0;
+while(j<arr.length){
+    let sum=arr[0];
+    while(sum<=n && j<arr.length){
+        sum=sum+arr[j];
+         x[i].push(arr[j]);
+        j++;
+    }
+    i++;
+}
+return x;
 };
 
 const n = prompt("Enter n: ");
